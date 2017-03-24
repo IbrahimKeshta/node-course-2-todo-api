ebf00cc5f96b108926e8caffd27d26119fda5d12
IbrahimKeshta <keshta95@hotmail.com>
2017-03-24 20:11:46 +0200

    add post todo route and refactor mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String,
+        required: true, // it should be entered
+        minlength: 1, // it should at least one letter
+        trim: true // trim spaces in beggining of the word
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    completedAt: {
+        type: Number,
+        default: null
+    }
+});
+
+module.exports = {Todo};
---
+++ server/server.js
@@ -0,0 +1,88 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text,
+        completed: req.body.completed
+    });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('started on port 3000');
+});
+
+
+                                                // Testing // 
+// var newTodo = new Todo({
+//     text: 'Cook dinner'
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log('Save todo', doc);
+// }, (e) => {
+//     console.log('Unable to save todo');
+// });
+
+// var newTodo = new Todo({
+//     text: '  Creating new model '
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log('Saved todo', doc);
+// }, (e) => {
+//     console.log('Unable to save todo', e);
+// });
+
+// challenge
+// user model -> email - password
+// email - require it - trim it - set type - set min length of 1
+
+
+
+// var newUser = new User({
+//     email: '     keshta95@hotmail.com  ',
+//     password: '123'
+// });
+
+// newUser.save().then((doc) => {
+//     console.log('User created', doc);
+// }, (e) => {
+//     console.log('Unable to create user', e);
+// });
