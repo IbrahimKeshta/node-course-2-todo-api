b02e720ed3f5ec6e0e2bd2b4d37daaf3686928ce
IbrahimKeshta <keshta95@hotmail.com>
2017-04-24 01:52:50 +0200

    Add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,46 @@
+const {ObjectID} = require('mongodb');
+
+const { mongoose } = require('./../server/db/mongoose');
+const { Todo } = require('./../server/models/todo');
+const { User } = require('./../server/models/user');
+
+
+// var id = '58fd227854374b4afcc1150f11';
+// if (!ObjectID.isValid(id)) {
+//     console.log('id not valid');
+// }
+// Todo.find({
+//     _id: id // mongoose can convert string id to object id 
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id // mongoose can convert string id to object id 
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('Id not found');
+//     }
+//     console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+/**
+ * Challenge
+ * query users collection
+ * handle the three cases
+ * *user not found
+ * *user found
+ * *handle any error
+ * load user model
+ */
+var userId = '58d30b9c7dba1726a413306f';
+User.findById(userId).then((user) => {
+    if (!user) {
+        return console.log('user not found'); //case 1
+    }
+    console.log('User By Id',JSON.stringify(user, undefined, 2)); // case 2
+}).catch((e) => console.log(e)); // case 3
