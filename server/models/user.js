211ddcec0ba314da05cd904305629d861586d34e
IbrahimKeshta <keshta95@hotmail.com>
2017-08-15 02:46:39 +0200

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -38,6 +38,7 @@ UserSchema.methods.toJSON = function () { // what exactly gets sent back when mo
 
     return _.pick(userObject, ['_id', 'email']);
 };
+
 UserSchema.methods.generateAuthToken = function () { //instance method called with indvidual document   
     var user = this;
     var access = 'auth';
@@ -50,60 +51,70 @@ UserSchema.methods.generateAuthToken = function () { //instance method called wi
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+    //$pull remove items from the arrays that match certain criteria
+    return user.update({
+        $pull: {
+            tokens: { token }
+        }
+    });
+};
+
 UserSchema.statics.findByToken = function (token) { // model method called with model 
     var User = this;
     var decoded;
 
     try {
-       decoded = jwt.verify(token, 'abc123');
+        decoded = jwt.verify(token, 'abc123');
     } catch (e) {
         return Promise.reject();
     }
 
     return User.findOne({
-         '_id': decoded._id,
-       'tokens.token': token,
-       'tokens.access': 'auth' 
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
     });
 };
 
 UserSchema.statics.findByCredentials = function (email, password) {
     var User = this;
 
-    return User.findOne({email}).then((user) => {
-        if(!user) {
-           return Promise.reject(); 
+    return User.findOne({ email }).then((user) => {
+        if (!user) {
+            return Promise.reject();
         }
 
         return new Promise((resolve, reject) => {
             // use bycrypt.compare to compare  password and user.password
             bcrypt.compare(password, user.password, (err, res) => {
-                if(res) {
+                if (res) {
                     resolve(user);
-                } else { 
+                } else {
                     reject();
-                } 
+                }
             });
         });
-    }); 
+    });
 };
 
 UserSchema.pre('save', function (next) {
     var user = this;
 
-  if (user.isModified('password')) {
-    //user.password
-    // user.password = hash;
-    //next();
-     bcrypt.genSalt(10, (err, salt) => {
-         bcrypt.hash(user.password, salt, (err, hash) => {
-             user.password = hash;
-             next();
-         });
-     });
-  } else {
-      next();
-  }
+    if (user.isModified('password')) {
+        //user.password
+        // user.password = hash;
+        //next();
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
 });
 var User = mongoose.model('User', UserSchema);
 
